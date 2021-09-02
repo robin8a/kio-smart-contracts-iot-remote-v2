a17fa402856e3cd24af869906174fa83f4cac5cf
ROBIN OCHOA <robin8a@local.mac>
2021-09-02 11:30:15 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # kio-smart-contracts-iot-remote-v2
 New IoT remote on cardano 
+
+# Run Pub/Sub
+```sh
+cd node/pub_sub
+## just first time
+# npm install 
+## just first time
+# chmod 777 start.sh 
+./start.sh
+```
---
+++ node/pub_sub/start.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# stop script on error
+set -e
+
+# run pub/sub sample app using certificates downloaded in package
+printf "\nRunning pub/sub sample application...\n"
+
+## Run modified app.js 
+# node app.js \
+# --host-name=az6wto8a6h0jn-ats.iot.us-east-1.amazonaws.com \
+# --private-key=certs/kio-smart-contracts-iot-device/kio-smart-contracts-iot-device.private.key \
+# --client-certificate=certs/kio-smart-contracts-iot-device/kio-smart-contracts-iot-device.cert.pem \
+# --ca-certificate=certs/kio-smart-contracts-iot-device/root-CA.crt \
+# --client-id=sdk-nodejs-e57c917f-032c-4778-8184-69116bc19f76
+
+node dist/index.js \
+--endpoint az6wto8a6h0jn-ats.iot.us-east-1.amazonaws.com \
+--ca_file certs/root-CA.crt \
+--cert certs/kio-smart-contracts-iot-device.cert.pem \
+--key certs/kio-smart-contracts-iot-device.private.key
